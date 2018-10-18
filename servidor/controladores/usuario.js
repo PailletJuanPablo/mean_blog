9c2c90d285d695a8c30626fd389211154b0b299c
PailletJuanPablo <pailletjuanpablo@gmail.com>
2018-10-18 18:23:43 -0300

    Refactored creating user

--- servidor/controladores/usuario.js
+++ servidor/controladores/usuario.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // Importamos modelo de usuario y bcrypt para encriptar contraseñas
-var User = require('../modelos/usuario')
+var Usuario = require('../modelos/usuario')
 var bcrypt = require('bcrypt');
 // Definimos la cantidad de repeticiones del encriptado
 const saltRounds = 10
@@ -21,57 +21,59 @@ function prueba (req, res) {
 // CREAR USUARIO
 
 let crearUsuario = (req, res) => {
-  var user = new User()
-  var params = req.body
-
-  console.log(params)
-
-  // Tomamos los valores desde los params del body
-  user.nombre = params.nombre
-  user.email = params.email
-
+  var usuario = new Usuario()
+  var body = req.body
+  console.log(body)
+  if(!body.nombre || !body.email){
+    return res.send({message:"Introduce email y nombre"})
+  }
+  // Tomamos los valores desde los body del body
+  usuario.nombre = body.nombre
+  usuario.email = body.email
   // Verificamos que la constraseña se haya enviado en request,
   // caso contrario devolvemos un mensaje de error
-  if (!params.password) {
-    return res.status(202).send({ message: 'Introduce contraseña' })
+  if (!body.password) {
+    return res.status(202).send({ mensaje: 'Introduce contraseña' })
   }
   // Si se recibio encriptamos contraseña
-  user.password = bcrypt.hashSync(params.password, saltRounds)
+  usuario.password = bcrypt.hashSync(body.password, saltRounds);
+  usuario.save().then((usuarioGuardado)=>{
+    return res.send(usuarioGuardado)
+  })
 }
 
 // LOGUEAR USUARIO
 
 let login = (req, res) => {
   // Recibimos los parametros del body con body-parser
-  let params = req.body
+  let body = req.body
 
-  let email = params.email
-  let password = params.password
-
-  User.findOne({ email: email.toLowerCase() }, (err, user) => {
-    // En primer lugar buscamos si existe un usuario con ese email
-    if (err) {
-      res.status(500).send({ message: 'Error en peticion' })
-    } else {
-      if (!user) {
-        res.status(404).send({ message: 'El usuario no existe' })
-      } else {
-        // Si existe, comprobamos la contraseña
-        bcrypt.compare(password, user.password, (err, check) => {
-          if (check) {
-            // Si el check es correcto, se devuelven los datos del user ahora logueado
-            res.status(200).send({
-              token: jwt.createToken(user)
-            })
-          } else {
-            res
-              .status(404)
-              .send({ message: 'El usuario no ha podido loguearse' })
-          }
-        })
-      }
+  let email = body.email
+  let password = body.password
+// Buscamos un usuario en base al email que recibimos del body
+  Usuario.findOne({email: email.toLowerCase()})
+  // si se realiza la busqueda se ejecuta un then
+  // Posteriormente, devolverá un false si no se encontró usuario
+  .then((usuarioEncontrado)=>{
+    // Si no se encontro usuario (usuarioEncontrado == false)
+    if (!usuarioEncontrado){
+      return res.send({mensaje:"No existe usuario"})
     }
-  })
+    // Sino, continuamos
+    // Comparamos la contraseña enviada en el body con la que tenemos guardada en base de datos (ambas encriptadas)
+    bcrypt.compare(password,usuarioEncontrado.password)
+    .then((passOk)=>{
+      if(passOk){
+        // Si salió todo bien, enviamos el JWT del usuario, que le permitirá loguearse
+        return res.status(200).send({
+          token: jwt.createToken(usuarioEncontrado)
+        })
+      }else{
+        // Sinó, la contraseña no es correcta
+        return res.status(400).send({message:"Contraseña incorrecta"})
+      }
+    })
+  });
 }
 
 // ACTUALIZAR USUARIO
